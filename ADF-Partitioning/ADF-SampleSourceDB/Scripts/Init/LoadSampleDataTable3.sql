d7529bd98233df5076b29f5a92351c780bbea938
Tomasz Krawczyk <tkrawczyk@future-processing.com>
2018-06-07 14:08:36 +0200

    Created sample source DB

---
+++ ADF-Partitioning/ADF-SampleSourceDB/Scripts/Init/LoadSampleDataTable3.sql
@@ -0,0 +1,18 @@
+﻿DECLARE @daysCount INT = 100;
+DECLARE @endDate DATETIME2 = DATEADD(DAY, - 1, SYSDATETIME());
+DECLARE @startDate DATETIME = DATEADD(DAY, - @daysCount, @startDate);
+
+IF (
+		SELECT COUNT(*)
+		FROM dbo.Table3
+		) = 0
+BEGIN
+	INSERT INTO dbo.Table3 (
+		[Value]
+		,InsertedOn
+		)
+	SELECT Number AS [Value]
+		,DATEADD(SECOND, number, @startDate) AS InsertedOn
+	FROM dbo.Numbers
+	WHERE DATEADD(SECOND, number, @startDate) < @endDate
+END
---
+++ ADF-Partitioning/ADF-SampleSourceDB/Scripts/LoadIncDataTable2.sql
@@ -0,0 +1,11 @@
+﻿DECLARE @startDate DATETIME2 = DATEADD(DAY, - 1, SYSDATETIME());
+DECLARE @endDate DATETIME = SYSDATETIME();
+
+INSERT INTO dbo.Table2 (
+	[Value]
+	,InsertedOn
+	)
+SELECT TOP 100 Number AS [Value]
+	,DATEADD(SECOND, number, @startDate) AS InsertedOn
+FROM dbo.Numbers
+ORDER BY Number ASC
